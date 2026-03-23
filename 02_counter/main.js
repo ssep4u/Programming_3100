21aa4a40f0cace39406d6e74d26e861f0b3f7ee2
ssep <ssep4u@gmail.com>
2026-03-23 14:14:45 +0900

    counter JS onclick

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -5,7 +5,11 @@ const resultH1 = document.getElementsByTagName("h1")[0];
 const plusButton = document.getElementsByTagName("button")[0];
 
 // button 누르면 숫자 증가하자
-plusButton.addEventListener("click", () => {
+// plusButton.addEventListener("click", () => {
+//     count++;    //count+=1; //count = count + 1;
+//     resultH1.innerHTML = count; // 증가한 숫자 화면에 표시하자
+// });
+plusButton.onclick = () => {
     count++;    //count+=1; //count = count + 1;
     resultH1.innerHTML = count; // 증가한 숫자 화면에 표시하자
-});
+};
