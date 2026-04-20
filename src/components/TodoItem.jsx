dcd06682be7d7bd5e8f2dd8d404aa2c7208cde8a
ssep <ssep4u@gmail.com>
2026-04-20 13:41:50 +0900

    TodoListApp show todos

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,11 @@
 import Checkbox from './Checkbox.jsx'
 import Button from './Button.jsx'
 
-export default function TodoItem() {
+export default function TodoItem({ todo }) {
     return (
-        <li className='todo__item todo__item--complete'>
-            <Checkbox id="1">할 일1</Checkbox>
+        // todo.isCompleted가 true 면 " todo__item--complete", false 면 ""
+        <li className={`todo__item${todo.isCompleted?" todo__item--complete":""}`}>
+            <Checkbox id={todo.id}>{todo.text}</Checkbox>
             <Button className='todo__button todo__button--edit'>✏️</Button>
             <Button className='todo__button todo__button--delete'>❌</Button>
         </li>
