3eef7f6fcdaac98bc593459efd2fd48e3feda0bf
전희진 <hijeon80@gmail.com>
2026-05-31 17:27:36 +0900

    1-2 3115: 배경색 변경

--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -1,5 +1,21 @@
-export default function TodoHeader() {
+import Button from './Button.jsx'
+import { useState } from 'react'
+
+export default function TodoHeader({ changeBgColor, currentBgColor }) {
+    const [pickerColor, setPickerColor] = useState(currentBgColor);
+
     return (
-        <h1 className='todo__title'>ToDo List</h1>
+        <>
+            <h1 className='todo__title'>ToDo List</h1>
+            <input
+                type="color"
+                value={pickerColor}
+                onChange={(e) => {
+                    setPickerColor(e.target.value);
+                    changeBgColor(e.target.value);
+                }}
+            />
+
+        </>
     )
 }
